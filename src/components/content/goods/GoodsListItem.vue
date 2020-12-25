7701a61b099c1ac72877d0bb41a837374c3dc9eb
Luo-Zhigang <2483762760@qq.com>
2020-12-25 19:27:01 +0800

    git

---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="goodsListItem">
+    <img :src="goodsItem.show.img"/>
+    <div>
+      <p>{{goodsItem.title}}</p>
+      <span class="price">{{goodsItem.price}}</span>
+      <span class="price">{{goodsItem.cfav}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "GoodsListItem",
+    props:{
+      goodsItem:{
+        type:Object,
+        default(){
+          return {};
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/content/tabControl/TabControl.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="tab_control">
+    <div v-for="(item,index) in titles"
+         class="tab-control-item"
+         :class="{active:index===currentIndex}" @click="itemClick(index)">
+      <span>{{item}}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "TabControl",
+    props: {
+      titles: {
+        type: Array,
+        default: function () {
+          return [];
+        }
+      }
+    },
+    data() {
+      return {
+        currentIndex: 0
+      }
+    },
+    methods:{
+      itemClick:function (index) {
+        this.currentIndex=index;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  #tab_control {
+    display: flex;
+    text-align: center;
+    font-size: 15px;
+    background-color: #fff;
+  }
+
+  .tab-control-item {
+    flex: 1;
+    height: 40px;
+    line-height: 40px;
+  }
+  .tab-control-item span{
+    padding: 4px;
+  }
+  .active{
+    color: var(--color-high-text);
+  }
+  .active span{
+    border-bottom: 3px solid var(--color-tint);
+  }
+</style>
